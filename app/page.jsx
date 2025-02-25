698a3e8790321e5376296ec6f37441cdd8aa31fc
Deviskalo <deviskalo2000@gmail.com>
2025-02-25 08:19:15 +0000

    feat: Integrate shadcn/ui and implement button component
    - Initialized shadcn/ui for enhanced UI components
    - Added Button component with styling and variants
    - Updated homepage to include a "Get Started" button
    - Configured Tailwind CSS for improved styling and theming

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="items-center flex justify-center h-full w-full flex-1 mt-80">
+      <Button>Get Started</Button>
+    </div>
+  );
+};
+
+export default Home;
