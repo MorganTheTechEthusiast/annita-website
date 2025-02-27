303935e6a3e540105b1c85e60820953ed29d35b3
Deviskalo <deviskalo2000@gmail.com>
2025-02-27 10:44:50 +0000

    Update HeroSection to be inspired by Flutterwave design

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -5,17 +5,18 @@ import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <section className="relative h-[100%] dark:bg-black/20 dark:text-gray-200 bg-gray-200/20 text-gray-800 py-20">
+    <section className="relative h-[100%] bg-gradient-to-r from-vibrant-orange to-vibrant-yellow text-gray-100 py-20">
       <div className="container mx-auto text-center relative z-10">
-        <h1 className="text-4xl md:text-5xl font-bold mb-4">Meet Annita</h1>
+        <h1 className="text-4xl md:text-5xl font-bold mb-4">
+          Welcome to Annita
+        </h1>
         <p className="text-lg md:text-xl mb-6">
           Africa's First all-in-one platform,
-          <br /> combining e-commerce, Fintech, Artificial Intelligence,
-          Communication, Marketing, Logistics, and lifestyle services
+          <br /> combining e-commerce, Fintech, AI, and lifestyle services.
         </p>
         <Link
           href="/marketplace"
-          className="bg-slate-100 text-vibrant-orange py-3 px-6 rounded-md font-semibold hover:bg-gray-200 border-black/20 border transition cursor-pointer"
+          className="bg-white text-vibrant-orange py-3 px-6 rounded-md font-semibold hover:bg-gray-200 transition cursor-pointer"
         >
           Explore Marketplace
         </Link>
@@ -26,7 +27,7 @@ const HeroSection = () => {
           alt="Hero Background"
           layout="fill"
           objectFit="cover"
-          className="opacity-20"
+          className="opacity-30"
         />
       </div>
     </section>
